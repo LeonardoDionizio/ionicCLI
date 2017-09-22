7a138f27e2e9d864261403d8bd8da2a75ff7bbd8
Leonardo Dionizio <leodionizio13@gmail.com>
2017-09-22 16:52:06 -0300

    algumas configurações de estilização e exibição dos usuarios em cards na tela

--- src/directives/background/background.directive.ts
+++ src/directives/background/background.directive.ts
@@ -6,8 +6,8 @@ import { Directive, ElementRef } from '@angular/core';
 export class BackgroundDirective {
 
   constructor(public el: ElementRef) {
-    this.el.nativeElement.style.backgroundColor = 'darkGray';
-    this.el.nativeElement.style.color = 'lightgray';
+    this.el.nativeElement.style.backgroundColor = 'lightgray';
+    this.el.nativeElement.style.color = '#000';
   }
 
 }
--- src/pages/dynamic/dynamic.ts
+++ src/pages/dynamic/dynamic.ts
@@ -1,6 +1,7 @@
 import { GithubProvider } from './../../providers/github/github.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,22 +9,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'dynamic.html',
 })
 export class DynamicPage {
+  public gitUsers: {};
+  public loader: any;
+ 
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public githubService: GithubProvider
+    public githubService: GithubProvider,
+    public loadingCtrl: LoadingController
   ) {
   }
 
   ionViewDidLoad() {
-    this.githubService.getUsers().subscribe((users: [{}]) => {
-      console.log(users);
-    })
+
+  }
+
+  presentLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Getting...",
+    });
+    this.loader.present();
   }
 
   public onIncrement(): void {
-    console.log(this.githubService.increment());
+    this.presentLoading();
+    this.githubService.getUsers().subscribe((users: [{}]) => {
+      this.gitUsers = users;
+      this.loader.dismiss();
+    })
   }
 
 }
